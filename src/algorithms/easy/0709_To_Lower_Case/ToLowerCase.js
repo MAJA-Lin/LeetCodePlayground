9e51f968bc9b50d7abd734f25f3418ad91b94aa9
Scott Lin <MAJA-Lin@users.noreply.github.com>
2018-08-22 11:19:26 +0800

    Use build-in js library

--- src/algorithms/easy/0709_To_Lower_Case/ToLowerCase.js
+++ src/algorithms/easy/0709_To_Lower_Case/ToLowerCase.js
@@ -3,17 +3,5 @@
  * @return {string}
  */
 var toLowerCase = function (str) {
-    const UPPER_A = 65;
-    const UPPER_Z = 90;
-    let result = str.split('');
-
-    for (let i = 0; i < str.length; i++) {
-        let ascii = str.charCodeAt(i)
-        if (ascii >= UPPER_A && ascii <= UPPER_Z) {
-            result[i] = String.fromCharCode(ascii + 32)
-        }
-    }
-    result = result.join('');
-
-    return result;
+    return str.toLocaleLowerCase();
 };
