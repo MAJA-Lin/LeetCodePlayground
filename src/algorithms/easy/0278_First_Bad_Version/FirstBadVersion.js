aff99f957d4740217b09c21d3aab53ae6c177788
Scott Lin <josh.al.carrer@gmail.com>
2019-04-12 16:14:33 +0800

    Add 0278: First Bad Version js version

---
+++ src/algorithms/easy/0278_First_Bad_Version/FirstBadVersion.js
@@ -0,0 +1,71 @@
+/**
+ * Definition for isBadVersion()
+ *
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * Linear search
+ *
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var linearSearchSolution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        for (let i = 1; i <= n; i++) {
+            if (isBadVersion(i)) {
+                return i;
+            }
+        }
+    };
+};
+
+/**
+ * Binary search
+ *
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        // Edge case
+        if (isBadVersion(1)) {
+            return 1;
+        }
+
+        let left = 1;
+        let right = n;
+        let index = 0;
+
+        while (1) {
+            index = Math.ceil((left + right) / 2);
+            // console.log("Index", index)
+            // console.log("L:", left, "R: ", right)
+
+            if (isBadVersion(left) == false && isBadVersion(index) == false) {
+                left = index;
+                continue;
+            }
+
+            if (isBadVersion(left) == false && isBadVersion(index) == true) {
+                if (left == (index - 1)) {
+                    return index;
+                }
+                right = index;
+                continue;
+            }
+        }
+    };
+};
