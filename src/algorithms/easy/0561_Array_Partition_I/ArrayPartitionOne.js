9dd6949ea16edca6f8822f6ec3768ad63d7a616e
Scott Lin <josh.al.carrer@gmail.com>
2019-04-15 02:51:39 +0800

    Use closure and sort and improve speed

--- src/algorithms/easy/0561_Array_Partition_I/ArrayPartitionOne.js
+++ src/algorithms/easy/0561_Array_Partition_I/ArrayPartitionOne.js
@@ -3,23 +3,15 @@
  * @return {number}
  */
 var arrayPairSum = function(nums) {
-    let length = nums.length;
-    let sum = 0;
-    let tmp = 0;
-
-    for (let i = 0; i < length; i++) {
-        for (let j = i + 1; j < length; j++) {
-            if (nums[i] > nums[j]) {
-                tmp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = tmp;
+    return nums
+        .sort(function (a, b) {
+            return a - b;
+        })
+        .reduce(function (sum, current, i) {
+            if (i % 2 == 0) {
+                sum = sum + current;
             }
-        }
-    }
 
-    for (i = 0; i < length / 2; i++) {
-        sum = sum + nums[2 * i];
-    }
-
-    return sum;
+            return sum;
+        });
 };
