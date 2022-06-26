c3e032b9d2f023b4a487b0a344f30766c218e832
Scott Lin <scott.maja.lin@gmail.com>
2022-06-27 01:34:13 +0800

    feat: add dp solution

--- src/algorithms/easy/0121_Best_Time_Buy_And_Sell_Stock/BuyAndSellStock.go
+++ src/algorithms/easy/0121_Best_Time_Buy_And_Sell_Stock/BuyAndSellStock.go
@@ -3,10 +3,10 @@ package main
 func main() {
 	testCaseAlpha := []int{7, 1, 5, 3, 6, 4}
 
-	maxProfit(testCaseAlpha)
+	dpMaxProfit(testCaseAlpha)
 }
 
-func maxProfit(prices []int) int {
+func brutalMaxProfit(prices []int) int {
 	var max int = 0
 	for i := 0; i < len(prices)-1; i++ {
 		for j := i + 1; j < len(prices); j++ {
@@ -18,5 +18,28 @@ func maxProfit(prices []int) int {
 		}
 	}
 	// fmt.Printf("%v", max)
+	return max
+}
+
+/**
+ * Dynamic programming solution
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/discuss/1735550/Python-Javascript-Easy-solution-with-very-clear-Explanation
+ */
+func dpMaxProfit(prices []int) int {
+	i := 0
+	j := 1
+	max := 0
+	for j < len(prices) {
+		if prices[j] > prices[i] {
+			currentProfit := prices[j] - prices[i]
+			if currentProfit > max {
+				max = currentProfit
+			}
+		} else {
+			i = j
+		}
+		j++
+	}
+
 	return max
 }
